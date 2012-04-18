1ec33bba1f04d2f7b37dfbde04e14f261b24d788
Yin Poon <yhangpoon@gmail.com>
2012-04-18 01:43:06 -0400

    Added comments.

--- Semaphore/src/Driver.java
+++ Semaphore/src/Driver.java
@@ -1,11 +1,15 @@
 /**
- * @author Yin
+ * Driver class to test ReentrantSemaphoreLock and ReentrantLock.
  * 
+ * @author Yin
  */
 public class Driver {
 
     /**
+     * Main method to start the program.
+     * 
      * @param args
+     *            standard input arguments
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
--- Semaphore/src/ReentrantSemaphoreLock.java
+++ Semaphore/src/ReentrantSemaphoreLock.java
@@ -1,17 +1,30 @@
+import java.util.Date;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
 /**
- * @author Yin
+ * A reentrant mutual exclusion lock implemented using semaphore.
  * 
+ * @author Yin
  */
 public class ReentrantSemaphoreLock implements Lock {
 
-    /*
-     * (non-Javadoc)
+    private Semaphore lock;
+
+    /**
+     * Default Constructor
      * 
-     * @see java.util.concurrent.locks.Lock#lock()
+     * @param permits
+     *            The semaphore value
+     */
+    public ReentrantSemaphoreLock(int permits) {
+        this.lock = new Semaphore(permits);
+    }
+
+    /**
+     * Acquires the lock.
      */
     @Override
     public void lock() {
@@ -19,10 +32,10 @@ public class ReentrantSemaphoreLock implements Lock {
 
     }
 
-    /*
-     * (non-Javadoc)
+    /**
+     * Acquires the lock unless the current thread is interrupted.
      * 
-     * @see java.util.concurrent.locks.Lock#lockInterruptibly()
+     * @throws InterruptedException
      */
     @Override
     public void lockInterruptibly() throws InterruptedException {
@@ -30,10 +43,8 @@ public class ReentrantSemaphoreLock implements Lock {
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.util.concurrent.locks.Lock#tryLock()
+    /**
+     * Acquires the lock only if it is free at the time of invocation.
      */
     @Override
     public boolean tryLock() {
@@ -41,11 +52,11 @@ public class ReentrantSemaphoreLock implements Lock {
         return false;
     }
 
-    /*
-     * (non-Javadoc)
+    /**
+     * Acquires the lock if it is free within the given waiting time and the
+     * current thread has not been interrupted.
      * 
-     * @see java.util.concurrent.locks.Lock#tryLock(long,
-     * java.util.concurrent.TimeUnit)
+     * @throws InterruptedException
      */
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
@@ -53,10 +64,8 @@ public class ReentrantSemaphoreLock implements Lock {
         return false;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.util.concurrent.locks.Lock#unlock()
+    /**
+     * Releases the lock.
      */
     @Override
     public void unlock() {
@@ -64,10 +73,8 @@ public class ReentrantSemaphoreLock implements Lock {
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.util.concurrent.locks.Lock#newCondition()
+    /**
+     * Returns a new Condition instance that is bound to this Lock instance.
      */
     @Override
     public Condition newCondition() {
@@ -75,4 +82,82 @@ public class ReentrantSemaphoreLock implements Lock {
         return null;
     }
 
-}
+    /**
+     * Internal class SemaphoreCondition.
+     */
+    private class SemaphoreCondition implements Condition {
+
+        /**
+         * Causes the current thread to wait until it is signaled or
+         * interrupted.
+         */
+        @Override
+        public void await() {
+
+        }
+
+        /**
+         * Causes the current thread to wait until it is signalled.
+         */
+        @Override
+        public void awaitUninterruptibly() {
+            // TODO Auto-generated method stub
+
+        }
+
+        /**
+         * Causes the current thread to wait until it is signalled or
+         * interrupted, or the specified waiting time elapses.
+         * 
+         * @throws InterruptedException
+         */
+        @Override
+        public long awaitNanos(long nanosTimeout) throws InterruptedException {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        /**
+         * Causes the current thread to wait until it is signaled or
+         * interrupted, or the specified waiting time elapses.
+         * 
+         * @throws InterruptedException
+         */
+        @Override
+        public boolean await(long time, TimeUnit unit) throws InterruptedException {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        /**
+         * Causes the current thread to wait until it is signalled or
+         * interrupted, or the specified deadline elapses.
+         * 
+         * @throws InterruptedException
+         */
+        @Override
+        public boolean awaitUntil(Date deadline) throws InterruptedException {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        /**
+         * Wakes up one waiting thread.
+         */
+        @Override
+        public void signal() {
+
+        }
+
+        /**
+         * Wakes up all waiting threads.
+         */
+        @Override
+        public void signalAll() {
+            // TODO Auto-generated method stub
+
+        }
+
+    } // SemaphoreCondition
+
+} // ReentrantSemaphoreLock
